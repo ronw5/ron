b7637d077777a4c8f54b7dd1d817c2e11309410a
ronw5 <101996405+ronw5@users.noreply.github.com>
2022-03-20 23:52:15 +0300

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,7 @@
+echo "# ron" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ronw5/ron.git
+git push -u origin main
